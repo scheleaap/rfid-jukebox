a25a07449d23a3a41e9966a2d420bb777094b282
scheleaap <git@wout.maaskant.info>
2024-12-23 21:07:52 +0100

    chore: General updating
    * Dependency updates
    * Adapt to new IntelliJ version
    * Remove obsolete reference to Java 8

--- build.sbt
+++ build.sbt
@@ -1,35 +1,35 @@
 Global / onChangedBuildSource := ReloadOnSourceChanges
 
-ThisBuild / scalaVersion := "2.13.5"
+ThisBuild / scalaVersion := "2.13.15"
 
 ThisBuild / javacOptions ++= Seq(
   "-source",
-  "1.8",
+  "11",
   "-target",
-  "1.8",
+  "11",
   "-Xlint"
 )
 
 ThisBuild / scalacOptions ++= ScalacOptions.scalac213Options
 
 ThisBuild / libraryDependencies ++= {
-  val sttpClient = "2.2.9"
-  val tinylog = "2.3.1"
+  val sttpClient = "2.3.0"
+  val tinylog = "2.3.2"
   Seq(
     "com.diozero" % "diozero-core" % "0.14",
-    "com.github.pureconfig" %% "pureconfig" % "0.15.0",
+    "com.github.pureconfig" %% "pureconfig" % "0.17.8",
     "com.softwaremill.sttp.client" %% "core" % sttpClient,
     "com.softwaremill.sttp.client" %% "play-json" % sttpClient,
     "com.softwaremill.sttp.client" %% "async-http-client-backend-monix" % sttpClient, // TODO Replace with httpclient-backend below once it contains Monix support
     //"com.softwaremill.sttp.client" %% "httpclient-backend" % sttpClient,
-    "io.monix" %% "monix" % "3.3.0",
+    "io.monix" %% "monix" % "3.4.1",
     // Logging
     "org.tinylog" % "tinylog-api" % tinylog,
     "org.tinylog" % "tinylog-impl" % tinylog,
     "org.tinylog" % "slf4j-tinylog" % tinylog,
-    "com.typesafe.scala-logging" %% "scala-logging" % "3.9.3",
+    "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5",
     // Testing
-    "org.scalatest" %% "scalatest" % "3.2.8" % "test"
+    "org.scalatest" %% "scalatest" % "3.2.19" % "test"
   )
 }
 
--- src/test/scala/info/maaskant/jukebox/StateMachineTest.scala
+++ src/test/scala/info/maaskant/jukebox/StateMachineTest.scala
@@ -25,95 +25,95 @@ class StateMachineTest extends AnyFlatSpec with Matchers {
   import stateMachine.{Paused, Playing, Stopped, Uninitialized}
 
   "Uninitialized, any" should "Stopped, SignalReady" in {
-    Uninitialized(Card.None) should be(Stopped, Some(Initialize))
+    Uninitialized(Card.None) shouldBe(Stopped, Some(Initialize))
   }
 
   "Stopped, nothing" should "Stopped, None" in {
     val originalState = Stopped
-    originalState(Card.None) should be(originalState, None)
+    originalState(Card.None) shouldBe(originalState, None)
   }
 
   "Stopped, album" should "Playing, Play" in {
-    Stopped(album1) should be(playing(album1), play(album1))
+    Stopped(album1) shouldBe(playing(album1), play(album1))
   }
 
   "Stopped, unknown card" should "Stopped, None" in {
     val originalState = Stopped
-    originalState(unknown) should be(originalState, None)
+    originalState(unknown) shouldBe(originalState, None)
   }
 
   "Stopped, shutdown" should "Stopped, Shutdown" in {
-    Stopped(Card.Shutdown) should be(Stopped, shutdown)
+    Stopped(Card.Shutdown) shouldBe(Stopped, shutdown)
   }
 
   "Stopped, stop" should "Stopped, None" in {
     val originalState = Stopped
-    originalState(Card.Stop) should be(originalState, None)
+    originalState(Card.Stop) shouldBe(originalState, None)
   }
 
   "Playing, nothing" should "Paused, Pause" in {
-    playing(album1)(Card.None, clock(instant1)) should be(paused(album1, instant1), pause)
+    playing(album1)(Card.None, clock(instant1)) shouldBe(paused(album1, instant1), pause)
   }
 
   "Playing, same card" should "Playing, None" in {
     val originalState = playing(album1)
-    originalState(album1) should be(originalState, None)
+    originalState(album1) shouldBe(originalState, None)
   }
 
   "Playing, different card" should "Playing, Play" in {
-    playing(album1)(album2) should be(playing(album2), play(album2))
+    playing(album1)(album2) shouldBe(playing(album2), play(album2))
   }
 
   "Playing, unknown card" should "Playing, None" in {
     val originalState = playing(album1)
-    originalState(unknown) should be(originalState, None)
+    originalState(unknown) shouldBe(originalState, None)
   }
 
   "Playing, shutdown" should "Playing, Shutdown" in {
     val originalState = playing(album1)
-    originalState(Card.Shutdown) should be(originalState, shutdown)
+    originalState(Card.Shutdown) shouldBe(originalState, shutdown)
   }
 
   "Playing, stop" should "Stopped, Stop" in {
-    playing(album1)(Card.Stop) should be(Stopped, stop)
+    playing(album1)(Card.Stop) shouldBe(Stopped, stop)
   }
 
   "Paused, nothing" should "Paused, None" in {
     val originalState = paused(album1, instant1)
-    originalState(Card.None, clock(instant2)) should be(originalState, None)
+    originalState(Card.None, clock(instant2)) shouldBe(originalState, None)
   }
 
   "Paused (HTTP URI), same card after short delay" should "Playing, Resume" in {
-    paused(httpAlbum, instant1)(httpAlbum, clock(instant1.plusSeconds(59))) should be(playing(httpAlbum), resume)
+    paused(httpAlbum, instant1)(httpAlbum, clock(instant1.plusSeconds(59))) shouldBe(playing(httpAlbum), resume)
   }
 
   "Paused (HTTP URI), same card after long delay" should "Playing, Play" in {
-    paused(httpAlbum, instant1)(httpAlbum, clock(instant1.plusSeconds(60))) should be(
+    paused(httpAlbum, instant1)(httpAlbum, clock(instant1.plusSeconds(60))) shouldBe(
       playing(httpAlbum),
       play(httpAlbum)
     )
   }
 
   "Paused (other), same card after long delay" should "Playing, Resume" in {
-    paused(album1, instant1)(album1, clock(instant1.plusSeconds(60))) should be(playing(album1), resume)
+    paused(album1, instant1)(album1, clock(instant1.plusSeconds(60))) shouldBe(playing(album1), resume)
   }
 
   "Paused, different card" should "Playing, Play" in {
-    paused(album1)(album2) should be(playing(album2), play(album2))
+    paused(album1)(album2) shouldBe(playing(album2), play(album2))
   }
 
   "Paused, unknown card" should "Paused, None" in {
     val originalState = paused(album1, instant1)
-    originalState(unknown, clock(instant2)) should be(originalState, None)
+    originalState(unknown, clock(instant2)) shouldBe(originalState, None)
   }
 
   "Paused, shutdown" should "Paused, Shutdown" in {
     val originalState = paused(album1, instant1)
-    originalState(Card.Shutdown) should be(originalState, shutdown)
+    originalState(Card.Shutdown) shouldBe(originalState, shutdown)
   }
 
   "Paused, stop" should "Stopped, Stop" in {
-    paused(album1)(Card.Stop) should be(Stopped, stop)
+    paused(album1)(Card.Stop) shouldBe(Stopped, stop)
   }
 
   private def clock(instant: Instant) = Clock.fixed(instant, zoneId)
